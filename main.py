7833192bf7cbec1d6da3aa4d219b99b0a513057d
Torsten Mirow <torsten.mirow@diva-e.com>
2018-10-25 17:28:19 +0200

    simple convert csv ; seperated to isinrisk.js + isinrisk.json for client

---
+++ main.py
@@ -0,0 +1,30 @@
+#!.venv/bin/python
+import csv
+import json
+
+with open('result.csv', 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=';')
+    headers = next(csvreader)
+    column = {}
+    rows = {}
+    for h in headers:
+        column[h] = []
+
+    for (i, row) in enumerate(csvreader):
+        json_key = ''
+        full_row = {}
+        for (j, column) in enumerate(row):
+            if (j == 0):
+                json_key = column.strip()
+            else:
+                header = headers[j]
+                full_row[header] = column.strip()
+
+        rows[json_key] = full_row
+
+
+with open('isinRisk.json', 'w') as outfile:
+    json.dump(rows, outfile)
+
+with open("isinRisk.js", 'a') as file:
+    file.write("window.isinRisk = %s" % rows)
